aa335ae8b60a0c2463400a8d88927867e4a18a80
bradygaster <bradyg@microsoft.com>
2019-09-26 13:16:53 +0530

    added gRPC service

---
+++ DurableLoans.LoanOfficerNotificationService/Services/LoanApplicationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DurableLoans.LoanOfficerNotificationService.Services
+{
+    [Route("api/[controller]")]
+    public class ReceiveLoan : Controller
+    {
+        [HttpPost]
+        public void Post([FromBody] LoanApplication loanApplication)
+        {
+        }
+    }
+}
---
+++ DurableLoans.LoanOfficerNotificationService/Services/LoanApplicationProxy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DurableLoans.LoanOfficerNotificationService.Services
+{
+    public class LoanApplicationProxy
+    {
+        public LoanApplicationProxy()
+        {
+        }
+    }
+}
